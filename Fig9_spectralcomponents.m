7a1dc9053af0c949ffe7c00a67c1abfc130399a3
Brian Wandell <wandell@stanford.edu>
2022-02-28 09:33:53 -0800

    Update Fig9_spectralcomponents.m

--- Fig9_spectralcomponents.m
+++ Fig9_spectralcomponents.m
@@ -3,15 +3,23 @@
 % Show the different spectral factors, and then show the fundamentals
 %
 
+%%
+wave = 400:1:700;
+
 %%  First the lens
 thisLens = Lens;
-wave = thisLens.get('wave');
+thisLens.set('wave',wave);
+
+% Base lens density
 lensDensity = thisLens.density;
+
 ieNewGraphWin;
 hold on;
-for dd = -1:0.2:1.4
-    thisLens.set('density',lensDensity + dd);
-    t = thisLens.get('transmittance');
+
+% Vary around the base
+for dd = -0.4:0.1:0.4
+    thisLens.density = lensDensity + dd;
+    t = thisLens.transmittance;
     p = plot(wave,t,'color',[0.5 0.5 0.5]);
     if dd == 0
         p.LineWidth = 2;
@@ -25,11 +33,14 @@ ylabel('Transmittance');
 %% Then the Macular pigment
 
 thisM = Macular;
-wave = thisM.wave;
+thisM.wave = wave;
+
+% Base
 macularDensity = thisM.density;
 
 ieNewGraphWin;
 hold on;
+% Vary around base
 for dd = -0.2:0.05:0.2
     thisM.density = dd + macularDensity;
     t = thisM.transmittance;
@@ -44,55 +55,106 @@ xlabel('Wavelength (nm)');
 ylabel('Transmittance');
 
 %% The cones
+
 conePigments = photoPigment;
+conePigments.wave = wave;
+% Base
 pigmentDensity = conePigments.opticalDensity;
 
 ieNewGraphWin;
 hold on;
-for dd=-0.2:0.1:0.2
+
+thisColor = [0.8 0.2 0.2; 0.2 0.6 0.2; 0.1 0.1 0.8];
+peakColor = {'r','g','b'};
+% Vary around base
+for dd=-0.1:0.01:0.1
     conePigments.opticalDensity = pigmentDensity + dd*ones(size(pigmentDensity));
-    p = plot(conePigments.wave,conePigments.absorptance,'color',[0.5 0.5 0.5]);
+    p = plot(conePigments.wave,conePigments.absorptance);
+    for ii=1:3
+        p(ii).Color = thisColor(ii,:);
+        p(ii).LineStyle = ':';
+    end
+    
     if dd==0
         for ii=1:3
             p(ii).LineWidth = 2;
-            p(ii).Color = 'k';
+            p(ii).Color = peakColor{ii};
+            p(ii).LineStyle = '-';
         end
     end
 end
+grid on; xlabel('Wavelength (nm)'); ylabel('Prob(Absorbtion)');
 
 %% Fundamentals
 
 % Build a mosaic
 cm = cMosaic;
+wave = 400:1:700;
+cm.wave = wave;
+thisLens.wave = wave;
 
-%% Default fundamentals
+%% Fundamentals and nearby, varying lens, macular, and pigment
 
-% Should we make some variations?
-
-cm.wave = 400:1:700;
-cm.plot('spectral qe');
-xlabel('Wavelength (nm)');
-ylabel('Quantal absorption');
-grid on;
-
-%%
-hdl = ieNewGraphWin; 
+hdl = ieNewGraphWin;
 hold on;
-thisLens = Lens;
-for ll = 1 %[-0.5 0 0.2]
-    thisLens.density = lensDensity + ll;
-    for mm = -0.3:0.1:0
-        cm.macular.density = macularDensity + mm;
-        [~,fighdl] = cm.plot('spectral qe',[],'lens',thisLens,'hdl',hdl);
-        if mm == 0
-            fighdl.Children.Children(end).Color = 'k';
-        else 
-            fighdl.Children.Children(end).Color = [0.5 0.5 0.5];
-        end
+thisColor = [0.6 0.2 0.2; 0.2 0.6 0.2; 0.1 0.2 0.5];
 
-        cm.macular.density
-        thisLens.density
+for ll = -0.2:0.1:0.2
+    thisLens.density = lensDensity + ll;
+    for mm = -0.2:0.1:0.2
+        cm.macular.density = macularDensity + mm;
+        for pp = -0.1:0.05:0.1
+            cm.pigment.opticalDensity = pigmentDensity + pp*ones(size(pigmentDensity));
+            [~,fighdl] = cm.plot('spectral qe',[],'lens',thisLens,'hdl',hdl);
+            p = fighdl.Children.Children;
+            
+            if ll==0 && mm == 0 && pp == 0
+                % The standard.
+                for ii=1:3
+                    p(ii).LineWidth = 2;
+                    p(ii).Color = peakColor{4-ii};                    
+                end
+            else
+                for ii=1:3
+                    p(ii).LineWidth = 0.5;
+                    p(ii).Color = thisColor(4-ii,:);
+                    p(ii).LineStyle = ':';
+                end
+            end
+        end
     end
 end
 
+%% Fundamentals with and without macular pigment
 
+hdl = ieNewGraphWin;
+
+thisColor = [0.6 0.2 0.2; 0.2 0.6 0.2; 0.1 0.1 0.6];
+
+cm.macular.density = 0.35;
+cm.plot('spectral qe',[],'lens',thisLens,'hdl',hdl);
+p = hdl.Children.Children;
+for ii=1:3
+    p(ii).LineWidth = 2;
+    %    p(ii).Color = thisColor(4-ii,:);
+    p(ii).Color = peakColor{4-ii};
+    
+    p(ii).LineStyle = '--';
+end
+
+hold on;
+cm.macular.density = 0;
+cm.plot('spectral qe',[],'lens',thisLens,'hdl',hdl);
+p = hdl.Children.Children;
+
+for ii=1:3
+    p(ii).LineWidth = 2;
+    %     p(ii).Color = thisColor(4-ii,:);
+    p(ii).Color = peakColor{4-ii};
+    
+    p(ii).LineStyle = '-';
+end
+
+% legend({'Density 0.35','Density 0'});
+
+%%
